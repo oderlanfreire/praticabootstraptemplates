4c4889b90b81a113b16eec3098334d6326755b32
oderlanfreire <oderlanfreire@gmail.com>
2023-06-06 09:10:58 -0300

    alterações finais

--- fusion/settings.py
+++ fusion/settings.py
@@ -141,3 +141,5 @@ DEFAULT_FROM_EMAIL = 'contact@yourdomain.com'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL= 'index'
